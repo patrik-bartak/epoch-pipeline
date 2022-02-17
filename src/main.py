9b6b4d08625881259cce26d6fd914c7d1735be68
Patrik Barták <p.bartak@student.tudelft.nl>
2022-02-17 04:45:06 +0100

    Fixing pylint issues

--- src/main.py
+++
@@ -1,7 +0,0 @@
-def some_calculation(x, y):
-    return x + y
-
-
-if __name__ == "__main__":
-    val = some_calculation(2, 5)
-    print(f"number {val}")
---
+++ src/main/main.py
@@ -0,0 +1,18 @@
+"""
+Module docstring
+"""
+
+
+def some_calculation(x, y):
+    """
+    method docstring
+    :param x: something
+    :param y: something
+    :return: something
+    """
+    return x + y
+
+
+if __name__ == "__main__":
+    VAL = some_calculation(2, 5)
+    print(f"number {VAL}")
